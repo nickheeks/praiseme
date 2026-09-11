79463611959a1865b545c8875e1b99fc9a10bd13
Nick Heeks <nick.heeks@gmail.com>
2026-09-12 00:39:10 +1000

    Initial commit

---
+++ R/praise.R
@@ -0,0 +1,19 @@
+#' Deliver praise
+#'
+#' This function is super useful when you're feeling sad
+#' and is useful for everyone.
+#'
+#'
+#' @param name A character string; the name of the person to praise
+#' @param punctuation A character string; emphasis for the praise
+#' @returns A character string with praise
+#' @export
+#'
+#' @examples
+#' praise("Thomas")
+#' praise("Alice", "!!!")
+praise <- function(name, punctuation = "!") {
+  glue::glue("You're the best, {name}{punctuation}")
+}
+
+praise("Thomas")
